1e319da8519613892c5822bbef92ba56b745f2b1
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-04 10:49:27 +0700

    add leap year

---
+++ src/LeapYearCaculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LeapYearCaculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập năm");
+        int year = scanner.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.printf("%d là năm nhuận", year);
+                } else {
+                    System.out.printf("%d không phải năm nhuận", year);
+                }
+            } else {
+                System.out.printf("%d là năm nhuận", year);
+            }
+        } else {
+            System.out.printf("%d  không phải năm nhuận", year);
+        }
+    }
+}
